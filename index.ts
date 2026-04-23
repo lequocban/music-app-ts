8f503b7c9b12750c43ca9fb5e601607b8b8f2cef
lequocban <lequocban2005@gmail.com>
2026-04-23 20:52:01 +0700

    tạo các mục đơn giản

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("ok")
+import express, { Express } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app: Express = express();
+const port: number | string = process.env.PORT || 5000;
+
+app.get("/topics", (req: express.Request, res: express.Response) => {
+  res.send("Chủ đề bài hát");
+});
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port} http://localhost:${port}`);
+});
