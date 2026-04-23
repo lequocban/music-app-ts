50a5886dc6b522e7859729eeb0e1ad2d101fc7a1
lequocban <lequocban2005@gmail.com>
2026-04-23 21:02:03 +0700

    kết nối database

--- index.ts
+++ index.ts
@@ -1,7 +1,10 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
+import * as database from "./config/database";
 
 dotenv.config();
+
+database.connect();
 
 const app: Express = express();
 const port: number | string = process.env.PORT || 5000;
