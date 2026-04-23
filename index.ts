b353a366e630d026dc00241a1abd0dbeab688782
lequocban <lequocban2005@gmail.com>
2026-04-23 21:09:39 +0700

    lấy ra chủ đề bài hát

--- index.ts
+++ index.ts
@@ -6,14 +6,23 @@ dotenv.config();
 
 database.connect();
 
+import Topic from "./models/topic.model";
+
 const app: Express = express();
 const port: number | string = process.env.PORT || 5000;
 
 app.set("views", `./views`);
 app.set("view engine", "pug");
 
-app.get("/topics", (req: express.Request, res: express.Response) => {
-  res.render("client/pages/topics/index");
+app.get("/topics", async (req: express.Request, res: express.Response) => {
+  try {
+    const topics = await Topic.find({ deleted: false });
+    console.log(topics);
+    res.render("client/pages/topics/index", { topics });
+  } catch (error) {
+    console.error("Error fetching topics:", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(port, () => {
